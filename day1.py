37802a1e8b8c46fa4fd4c6f4b0c56b0341c4016e
Matt Inwood <matt.inwood@gmail.com>
2019-12-01 17:59:35 -0600

    Added docstrings, type hints, and tests.

--- day1.py
+++ day1.py
@@ -1,35 +1,69 @@
 from time import time
 
 
-def read_input(filename):
-    return open(filename, 'r').read().split('\n')
+def read_input(filename) -> list:
+    '''
+    :param filename: The name of the file in format day{x}.txt where {x} is the day of the challenge
+    :return: List of values for the problem set
+    '''
+    return open('inputs/' + filename, 'r').read().split('\n')
 
 
-def fuel_req(m):
-    return (int(m)//3) - 2
+def fuel_requirements(mass: int) -> int:
+    '''
+    :param mass: The mass of the input for which to calculate fuel for
+    :return: The fuel required for the given mass.
+    '''
+    return (int(mass) // 3) - 2
 
 
-def problem_a():
-    mass = read_input('inputs/day1.txt')
-    mass = [fuel_req(x) for x in mass]
-    return sum(mass)
+def problem_a() -> int:
+    '''
+    Read the list of all module masses for input, and run the non-recursive function for determining fuel requirements.
+    :return: The total fuel requirements of all modules.
+    '''
+    modules = read_input('inputs/day1.txt')
+    fuel = [fuel_requirements(x) for x in modules]
+    return sum(fuel)
 
 
-def recursive_fuel_req(m):
-    f = (int(m)//3) - 2
-    if f > 6:
-        f += recursive_fuel_req(f)
-    return f
+def recursive_fuel_requirements(mass: int) -> int:
+    '''
+    :param mass: The mass of the input for which to calculate fuel for
+    :return: The fuel required for the given mass, as well as fuel required for the additional fuel.
+    '''
+    fuel = (int(mass)//3) - 2
+    # Any mass of 6 or less functionally requires zero fuel, and thus does not require any add'l fuel
+    if fuel > 6:
+        fuel += recursive_fuel_requirements(fuel)
+    return fuel
 
 
-def problem_b():
-    mass = read_input('inputs/day1.txt')
-    mass = [recursive_fuel_req(x) for x in mass]
-    return sum(mass)
+def problem_b() -> int:
+    '''
+    Read the list of all module masses for input, and run the recursive function for determining fuel requirements.
+    :return: The total fuel requirements of all modules, as well as the fuel requirements for the fuel.
+    '''
+    modules = read_input('inputs/day1.txt')
+    fuel = [recursive_fuel_requirements(x) for x in modules]
+    return sum(fuel)
+
+
+def test_fuel_requirements():
+    assert fuel_requirements(12) == 2
+    assert fuel_requirements(14) == 2
+    assert fuel_requirements(1969) == 654
+    assert fuel_requirements(100756) == 33583
+
+
+def test_total_fuel_requirements():
+    assert recursive_fuel_requirements(14) == 2
+    assert recursive_fuel_requirements(1969) == 966
+    assert recursive_fuel_requirements(100756) == 50346
 
 
 if __name__ == '__main__':
     s = time()
     print(f"Solution for Part A: {problem_a()}")
     print(f"Solution for Part B: {problem_b()}")
-    print(f"Completed in {time()-s} seconds")
+    print(f"Completed in {time()-s:.4f} seconds")
